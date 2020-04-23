cd0f31408b179bf39be96d574a7a8033d8b4633a
yoniKliger <yonikliger@gmail.com>
2020-04-23 11:05:38 +0300

    refactor(app): converting app to functional component

--- client/index.js
+++ client/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>Hello World</div>
-        )
-    }
-}
+const App = () => <div>Hello World</div>;
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
